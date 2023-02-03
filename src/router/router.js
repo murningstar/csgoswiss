545204f392bef282b1eb67d1180b1d444845e714
menyadeniskostin <menyadeniskostin@gmail.com>
2023-02-03 14:12:52 +0300

    Deleted separe mapViews from router

--- src/router/router.js
+++ src/router/router.js
@@ -1,19 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
-import mapsComponentsExport from "@/components/maps/mapsComponentsExport.js";
 
 let router = createRouter({
   history: createWebHistory(),
   routes: [{ path: "/", redirect: "/mirage" }],
   linkActiveClass: "mapLink-active",
-});
-
-mapsComponentsExport.forEach((mapComponent) => {
-	router.addRoute({
-		name: `/${mapComponent.name}`,
-		path: `/${mapComponent.name}`,
-		component: mapComponent,
-		meta: { mapId: mapComponent.mapId },
-	});
 });
 
 export default router;
