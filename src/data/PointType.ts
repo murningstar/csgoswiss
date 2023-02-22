c0de5026b502e04b2043b04863be9d4e43828930
menyadeniskostin <menyadeniskostin@gmail.com>
2023-02-22 04:58:06 +0300

    points_mirage rename to points_mirage_alpha

--- src/data/PointType.ts
+++ src/data/PointType.ts
@@ -1,43 +1,7 @@
 export type PointType = {
-	name: string;
-	position: {
-		x: number;
-		y: number;
-	};
-}
-enum GrenadeTypesEnum {
-    Smoke = "Smoke",
-    Molotov = "Molotov",
-    Flash = "Flash",
-    He = "He",
-}
-type CoordsObj = {
-    x: number;
-    y: number;
+    name: string;
+    position: {
+        x: number;
+        y: number;
+    };
 };
-type LinksObj = {
-    //Картинка откуда кидается
-	//Картинка куда целиться
-	//Картинка где взрывается
-	//Встроенный видос на ютуб
-};
-enum ThrowTypesEnum {
-
-}
-export class GrenadePoint {
-    type: GrenadeTypesEnum;
-    positionName: string;
-    coords: CoordsObj;
-    links: LinksObj;
-    constructor(
-        type: GrenadeTypesEnum,
-        positionName: string,
-        coords: CoordsObj,
-        links: LinksObj
-    ) {
-        this.type = type;
-        this.positionName = positionName;
-        this.coords = coords;
-        this.links = links;
-    }
-}
