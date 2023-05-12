353abd71f3b80d4867c4135597df6ec4857edf99
menyadeniskostin <menyadeniskostin@gmail.com>
2023-05-12 21:00:10 +0300

    Spot type reOpinionated

--- src/data/interfaces/Spot.ts
+++ src/data/interfaces/Spot.ts
@@ -4,6 +4,8 @@ export interface Spot {
     spotId: string;
     name: string;
     coords: CoordsObj;
-    toImgSrc: string | null;
-    fromImgSrc: string | null;
+    toSrc_1: string | null;
+    toSrc_2: string | null;
+    fromSrc_fp: string | null;
+    fromSrc_tp: string | null;
 }
