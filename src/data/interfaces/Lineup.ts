6c855edf31b996a233467013498781eea9bc1b95
menyadeniskostin <menyadeniskostin@gmail.com>
2023-05-12 22:54:41 +0300

    Lineup type update (+priorities)

--- src/data/interfaces/Lineup.ts
+++ src/data/interfaces/Lineup.ts
@@ -20,6 +20,9 @@ export interface Lineup {
     imgSrcAim?: string;
     imgSrcOverview?: string;
     imgSrcOverview2?: string;
+    /* priority: Если выбран 1, то overview будет 
+    отображаться изначально, а overview2 - onhover */
+    priority?: 1 | 2;
     side: Side;
     tickrate: Tickrate;
     comboLineupIds?: Lineup["lineupId"][];
